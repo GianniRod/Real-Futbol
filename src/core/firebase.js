23c40c7309b30ff109d5dcf230d73538b85062ec
Giancarlo <127577123+GianniRod@users.noreply.github.com>
2026-01-17 07:55:00 -0300

    Update firebase.js

--- src/core/firebase.js
+++ src/core/firebase.js
@@ -23,7 +23,9 @@ import {
     doc,
     getDoc,
     setDoc,
-    getDocs
+    getDocs,
+    deleteDoc,
+    updateDoc
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import {
     getAuth,
@@ -65,6 +67,8 @@ export {
     getDoc,
     setDoc,
     getDocs,
+    deleteDoc,
+    updateDoc,
     GoogleAuthProvider,
     signInWithPopup,
     signOut,
